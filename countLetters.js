db9530c40ef03a9e9e33014f809f2310a602e1c3
Whaddup <simonwong163@gmail.com>
2018-11-30 20:12:33 -0500

    Final code

--- countLetters.js
+++ countLetters.js
@@ -1,14 +1,15 @@
-var output = {}
-
-var letters = "abcdefgh"
-
-
-function countLetters(str){
-
-
-for (var i = 0; i < str.length; i++){
-  output[str[i]] += 1
+var output = {};
+function countLetters(str) {
+  var string = str.toLowerCase().split(' ').join('').split('');
+  for (var i = 0; i < string.length; i++) {
+    if (output[string[i]] === undefined) {
+      output[string[i]] = 1;
+    } else {
+      output[string[i]] += 1;
+    }
+  }
+console.log(output);
 }
-console.log(output)
+countLetters("lighthouse in the house");
 
-}
+
